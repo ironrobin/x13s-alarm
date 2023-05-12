9785fb4f68a60ff4b276cd97d8fd66133ffaf39c
Alex R <alex@ironrobin.net>
2023-05-12 12:31:46 -0500

    see how linux builds [no rel]

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ printf 'builduser ALL=(ALL) ALL\n' | tee -a /etc/sudoers
 cat ./gpg_key | base64 --decode | gpg --homedir /home/builduser/.gnupg --import
 rm ./gpg_key
 
-for i in x13s-touchscreen-udev alsa-ucm-conf-x13s x13s-firmware mesa-a690 linux-x13s linux-x13s-archiso; do 
+for i in linux-x13s-archiso; do 
 	status=13
 	git submodule update --init $i
 	cd $i

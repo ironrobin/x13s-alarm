84ed1ef95cb9b1689409f2cf0a603dba5a4f7472
Alex R <alex@ironrobin.net>
2023-12-12 18:07:01 -0600

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ printf 'builduser ALL=(ALL) ALL\n' | tee -a /etc/sudoers
 cat ./gpg_key | base64 --decode | gpg --homedir /home/builduser/.gnupg --import
 rm ./gpg_key
 
-for i in "linux-x13s" "mutter" "alsa-ucm-conf-git-x13s" "linux-x13s-archiso" "linux-x13s-rc" "mesa" "x13s-firmware" "x13s-touchscreen-udev x13s-tweaks" ; do
+for i in "linux-x13s" "mutter" "alsa-ucm-conf-git-x13s" "linux-x13s-archiso" "linux-x13s-rc" "mesa" "x13s-firmware" "x13s-touchscreen-udev" "x13s-tweaks" ; do
 	status=13
 	git submodule update --init $i
 	cd $i

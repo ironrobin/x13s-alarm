5253266961edd86c375c1196ed54937a192cef56
Alex R <alex@ironrobin.net>
2025-02-01 21:46:03 -0600

    fix build

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ repo_name=$(echo $repo_full | cut -d/ -f2)
 sed -i '/\[community\]/d' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 pacman-key --init
-pacman -Syu --noconfirm --needed sudo git wget python
+pacman -Syu --noconfirm --needed sudo git wget python which
 which python
 which python3
 sudo ln -s /usr/bin/python3 /usr/bin/python

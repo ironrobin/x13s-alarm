b84dcdbd21186e273f40ef63c4c0eb48dea0d914
Alex R <alex@ironrobin.net>
2025-06-10 08:22:48 -0500

    refresh gpg key

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,13 @@ repo_name=$(echo $repo_full | cut -d/ -f2)
 sed -i '/\[community\]/d' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 pacman-key --init
+cat ./gpg_key | base64 --decode | gpg --homedir /home/builduser/.gnupg --import
+rm ./gpg_key
+gpg --homedir /home/builduser/.gnupg --list-keys
+echo "refreshing key to pick up new expiration date signature"
+gpg --homedir /home/builduser/.gnupg --refresh-key 6ED02751500A833A
+gpg --homedir /home/builduser/.gnupg --list-keys
+exit 1
 pacman -Syu --noconfirm --needed sudo git wget python
 useradd builduser -m
 chown -R builduser:builduser /build

4a8bf4384b0a24b1544d940ced49357c4be4f925
Alex R <alex@ironrobin.net>
2025-08-01 16:39:39 -0500

    enable erofs for the archiso installer

--- linux-x13s/patch_kernel_config.py
+++ linux-x13s/patch_kernel_config.py
@@ -49,6 +49,7 @@ x13s_options = {
     "CONFIG_IP_NF_TARGET_NETMAP": "n",
     "CONFIG_IP_NF_TARGET_REDIRECT": "n",
     # Turn on regularly used filystems
+    "CONFIG_EROFS_FS": "y",
     "CONFIG_XFS_FS": "m",
     "CONFIG_XFS_SUPPORT_V4": "y",
     "CONFIG_XFS_SUPPORT_ASCII_CI": "y",

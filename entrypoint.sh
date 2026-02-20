a269f371621a38ca5b7552bd0104af9cc45b90e2
Alex Robinson <alex@ironrobin.net>
2026-02-20 12:10:10 -0600

    fix unknown trust pacman issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,7 @@ sed -i '/\[community\]/d' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 pacman-key --init
 pacman -Sy --noconfirm archlinux-keyring
-pacman -Syu --noconfirm
-pacman -S --needed sudo git wget python
+pacman -Syu --noconfirm --needed sudo git wget python
 useradd builduser -m
 chown -R builduser:builduser /build
 git config --global --add safe.directory /build
